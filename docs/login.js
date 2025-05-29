bafd0e5356c2e27b74df66f094a8ccd64378cf17
info <joelaal15@outlook.com>
2025-05-28 23:40:07 -0600

    cambios

--- docs/login.js
+++ docs/login.js
@@ -1,6 +1,6 @@
 const { createClient } = supabase;
 
-const supabase = createClient('https://npmhykkanlykzjsdzelo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbWh5a2thbmx5a3pqc2R6ZWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0ODUzMDksImV4cCI6MjA2MjA2MTMwOX0.3XFZdpYCV9DK60pogX0eM8OxmJtmAGdiDuWOaMv2H-Y');
+const supabaseClient = createClient('hhttps://npmhykkanlykzjsdzelo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbWh5a2thbmx5a3pqc2R6ZWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0ODUzMDksImV4cCI6MjA2MjA2MTMwOX0.3XFZdpYCV9DK60pogX0eM8OxmJtmAGdiDuWOaMv2H-Y');
 
 document.getElementById('login-form').addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -9,7 +9,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const contrasena = e.target.contrasena.value;
   const errorMsg = document.getElementById('error-msg');
 
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from('Usuarios')
     .select('contrasena_hash')
     .eq('usuario', usuario)
@@ -24,9 +24,8 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const match = await bcrypt.compare(contrasena, data.contrasena_hash);
 
   if (match) {
-    // Guardar sesión si lo deseas (ej. localStorage)
     localStorage.setItem('usuario', usuario);
-    window.location.href = 'dashboard.html';
+    window.location.href = 'admin.html';
   } else {
     errorMsg.textContent = 'Contraseña incorrecta';
     errorMsg.classList.remove('hidden');
